cf854bb4efcaec7081616620df39439d1065d7cf
George Cosmin Ilincuta <ilincuta.cosmin@gmail.com>
2021-04-09 19:57:15 +0300

    Add project files.

---
+++ CoreWithCSVHelper/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreWithCSVHelper.Models
+{
+    public class Student
+    {
+        [Index(0)]
+        public string Name { get; set; } = "";
+        [Index(2)]
+        public string Roll { get; set; } = "";
+        [Index(1)]
+        public string Email { get; set; } = "";
+    }
+}
